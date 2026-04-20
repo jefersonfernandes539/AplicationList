c69c27ff4259d290c9091551fe946f3094d3b8e9
jefersonfernandes539 <jeferson@almosthackers.io>
2026-04-20 06:24:57 -0300

    feat: implementacao de fluxo dos card

--- app/src/main/java/com/example/aplicationlist/domain/Todo.kt
+++ app/src/main/java/com/example/aplicationlist/domain/Todo.kt
@@ -1,3 +1,30 @@
 package com.example.aplicationlist.domain
 
-data class Todo()
+data class Todo(
+    val id: Long,
+    val title: String,
+    val description: String?,
+    val isCompleted: Boolean,
+)
+
+val todo1 = Todo (
+    id = 1,
+    title = "Title 1",
+    description = "Description 1",
+    isCompleted = false,
+)
+
+val todo2 = Todo (
+    id = 2,
+    title = "Title 2",
+    description = "Description 2",
+    isCompleted = true,
+)
+
+val todo3 = Todo (
+    id = 3,
+    title = "Title 3",
+    description = "Description 3",
+    isCompleted = false,
+)
+
--- app/src/main/java/com/example/aplicationlist/ui/feature/ListScreen.kt
+++ app/src/main/java/com/example/aplicationlist/ui/feature/ListScreen.kt
@@ -1,2 +1,75 @@
 package com.example.aplicationlist.ui.feature
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.aplicationlist.domain.Todo
+import com.example.aplicationlist.domain.todo1
+import com.example.aplicationlist.domain.todo2
+import com.example.aplicationlist.domain.todo3
+import com.example.aplicationlist.ui.components.TodoItem
+import com.example.aplicationlist.ui.theme.AplicationListTheme
+
+@Composable
+fun ListScreen() {
+    
+}
+
+@Composable
+fun ListContent(
+    todos: List<Todo>
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) {paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .consumeWindowInsets(paddingValues),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            itemsIndexed(todos) { index, todo ->
+                TodoItem(
+                    todo = todo,
+                    onCompletedChange = {},
+                    onItemClicked = {},
+                    onDeleteClick = {}
+                )
+
+                if (index < todos.lastIndex) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ListContentPreview() {
+    AplicationListTheme() {
+        ListContent(
+            todos = listOf(
+                todo1,
+                todo2,
+                todo3
+            )
+        )
+
+    }
+}
